bed9b4ff098344ae1eed6a404b39dbb78ed7db59
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-04-10 09:41:50 -0300

    Updated Component to acommodate the new Interface updates.

--- pyinterfacer/components/component.py
+++ pyinterfacer/components/component.py
@@ -5,7 +5,20 @@
 
 import pygame
 
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Literal
+
+default_component_types = Literal[
+    "animation",
+    "button",
+    "clickable",
+    "component",
+    "hoverable",
+    "image",
+    "input",
+    "spritesheet-animation",
+    "text-button",
+    "text",
+]
 
 
 class Component(pygame.sprite.Sprite):
@@ -21,6 +34,7 @@ class Component(pygame.sprite.Sprite):
         width: Optional[int] = 0,
         height: Optional[int] = 0,
         grid_cell: Optional[int] = None,
+        style: Optional[str] = None,
         groups: Tuple[pygame.sprite.AbstractGroup, ...] = (),
         *args,
         **kwargs,
@@ -35,14 +49,23 @@ class Component(pygame.sprite.Sprite):
         self.width = width if width is not None else 0
         self.height = height if height is not None else 0
         self.grid_cell = grid_cell
+        self.style_class = style
 
-        self.subtype = None  # Should be set by custom components, to indicate which of the default component set type the custom components belong to (used for group handling)
+        self.subtype: Optional[default_component_types] = (
+            None  # Should be set by custom components, to indicate which of the default component set type the custom components belong to (used for group handling)
+        )
 
     def preload_image(self) -> None:
-        """This method does nothing by default. Can be overwritten by inheriting classes to execute image preloading logic."""
+        """
+        This method does nothing by default. Can be overwritten by inheriting classes to execute image preloading logic.
+        Must be manually called.
+        """
         pass
 
     def update(self) -> None:
         self.image = pygame.Surface((self.width, self.height))
         self.rect = self.image.get_rect(center=(self.x, self.y))
         self.image.fill("black")
+
+    def __repr__(self) -> str:
+        return f"<{self.type.capitalize()} component | id: {self.id} ; interface: {self.interface} ; subtype: {self.subtype} ; in ({len(self.groups())}) groups>"
