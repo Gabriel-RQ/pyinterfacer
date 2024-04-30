671e78c8451e8e51708ac10d962b67efc7a48e2b
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-04-30 16:53:38 -0300

    Started refactoring PyInterfacer.

--- pyinterfacer/__init__.py
+++ pyinterfacer/__init__.py
@@ -1,5 +1,3 @@
 """
-PyInterfacer: A modular library designed to easily handle interfaces in pygame projects.
+PyInterfacer: A modular library designed to easily handle interfaces and components in pygame projects.
 """
-
-from .pyinterfacer import PyInterfacer, DefaultComponentTypes
